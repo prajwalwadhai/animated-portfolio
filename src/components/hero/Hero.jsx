93fcff0798b3339088fb84fb4e07f0a48edfdb4a
prajwal wadhai <prajwalwadhai55@gmail.com>
2023-10-20 21:37:00 +0530

    img updated

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -74,7 +74,7 @@ const Hero = () => {
         Writer Content Creator Influencer
       </motion.div>
       <div className="imageContainer">
-        <img src="/hero.png" alt="" />
+        <img src="/prajwal.png" alt="" />
       </div>
     </div>
   );
